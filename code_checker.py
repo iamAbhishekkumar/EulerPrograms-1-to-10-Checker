adeb2335ae9496750192763c5d4472648d459e17
Abhishek Kumar <53290881+iamAbhishekkumar@users.noreply.github.com>
2020-07-02 21:02:44 +0530

    Add files via upload

---
+++ code_checker.py
@@ -0,0 +1,81 @@
+import git
+import re
+from os import listdir, curdir
+from os.path import isfile, join, splitext
+
+import requests
+
+from dictionaries import language, euler_answer
+
+
+def compileCode(_lang, _code, _input="", save=False):
+    compile_url = "https://ide.geeksforgeeks.org/main.php"
+    data = {
+        'lang': _lang,
+        'code': _code,
+        'input': _input,
+        'save': save,
+    }
+    request = requests.post(url=compile_url, data=data)
+    code_sid = request.json()['sid']
+    return code_sid
+
+
+def getting_response(uid):
+    submit_url = "https://ide.geeksforgeeks.org/submissionResult.php"
+    submit_data = {
+        'sid': uid,
+        'requestType': "fetchResults"
+    }
+    submit = requests.post(url=submit_url, data=submit_data)
+    return submit.json()
+
+
+def getting_file_language(file_name):
+    name, file_extension = splitext(file_name)
+    return file_extension
+
+
+def checker(repo_link):
+    # Getting files from Repo
+    cloned_repo_dir = curdir + "/answers"
+    try:
+        git.Repo.clone_from(url=repo_link, to_path=cloned_repo_dir)
+    except git.exc.GitCommandError:
+        pass
+    finally:
+        files = [f for f in listdir(cloned_repo_dir) if isfile(join(cloned_repo_dir, f))]
+        filename_with_number = []
+        for filename in files:
+            num = re.findall(r'\d', filename)
+            if len(num) >= 1:
+                num = "".join(num)
+                try:
+                    filename_with_number.append([filename, num])
+                except KeyError:
+                    pass
+
+        # extracting required results
+        result = []
+        for i in filename_with_number:
+            if int(i[1]) <= 10:
+                filename = "answers/" + i[0]
+                print(filename + ".........")
+                code_file = open(filename, "r")
+                code = code_file.read()
+                lang = language[getting_file_language(filename)]
+                uid = compileCode(lang, code)
+                flag = 0
+                while flag == 0:
+                    try:
+                        output = getting_response(uid)['output']
+                        if output is not None:
+                            try:
+                                time = getting_response(uid)['time']
+                                result.append([output, euler_answer[i[1]], i[0], float(time)])
+                            except KeyError:
+                                pass
+                            flag = 1
+                    except KeyError:
+                        pass
+    return result
---
+++ mainFile.py
@@ -0,0 +1,43 @@
+from os import curdir, mkdir, path, walk, chmod
+from re import findall
+import stat
+import code_checker as cc
+import shutil
+
+cloned_repo_dir = curdir + "/answers"
+
+# deleting answer contents before cloning another repo
+if path.exists(cloned_repo_dir):
+    for root, dirs, files in walk(cloned_repo_dir):
+        for dir in dirs:
+            chmod(path.join(root, dir), stat.S_IRWXU)
+        for file in files:
+            chmod(path.join(root, file), stat.S_IRWXU)
+    shutil.rmtree(cloned_repo_dir)
+    mkdir(cloned_repo_dir)
+
+github_repo_link = input("Enter the repo link : ")
+result = cc.checker(github_repo_link)
+
+wrong_answer = []
+correct_answer = []
+total_time = 0
+
+for i in result:
+    if "".join(findall(r'\d', i[0])) == i[1]:
+        total_time += i[3]
+        correct_answer.append("yes")
+    else:
+        wrong_answer.append(i[2])
+
+if len(correct_answer) == 10 and total_time <= 5:
+    print("All are correct answers")
+elif total_time > 4:
+    print("Program takes too much time...")
+else:
+    if len(wrong_answer) == 0:
+        print("Some questions files may be missing.....")
+    else:
+        print("Following question number are wrong : ")
+        for _ in wrong_answer:
+            print(_)
